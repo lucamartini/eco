075a6f421673aead2da16119502125814767693e
Martini Luca <l.martini@nwgenergia.it>
2023-07-14 12:41:44 +0200

    use Link

--- src/app/Gallery/index.tsx
+++ src/app/Gallery/index.tsx
@@ -2,10 +2,11 @@ import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
 import HouseIcon from "@mui/icons-material/House";
 import ToysIcon from "@mui/icons-material/Toys";
 import PetsIcon from "@mui/icons-material/Pets";
-import CardAction from "./CardAction";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+import Link from "next/link";
+import CardActionArea from "@mui/material/CardActionArea";
 
 async function getItems() {
   return [
@@ -37,18 +38,20 @@ export default async function Gallery() {
       {items.map((item) => (
         <Grid key={item.id}>
           <Card sx={{ minWidth: 275 }}>
-            <CardAction id={item.id} key={item.id}>
-              <CardContent>
-                <Typography
-                  sx={{ fontSize: 14 }}
-                  color="text.secondary"
-                  gutterBottom
-                >
-                  {item.text}
-                </Typography>
-                {item.icon}
-              </CardContent>
-            </CardAction>
+            <CardActionArea>
+              <Link href={"/detail/" + item.id}>
+                <CardContent>
+                  <Typography
+                    sx={{ fontSize: 14 }}
+                    color="text.secondary"
+                    gutterBottom
+                  >
+                    {item.text}
+                  </Typography>
+                  {item.icon}
+                </CardContent>
+              </Link>
+            </CardActionArea>
           </Card>
         </Grid>
       ))}
